9f0c2d4099f35a74b1db77645fa21f26bf25e75b
rtarun9 <rtarun0913@gmail.com>
2024-12-26 06:59:01 +0530

    [WIP] core d3d12 object initialization and setup of render loop (clear color)

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include <format>
 #include <iostream>
 #include <source_location>
+#include <string>
 
 // DXGI / D3D12 includes.
 #include <d3d12.h>
@@ -39,6 +40,8 @@ static constexpr bool NETHER_DEBUG = true;
 static constexpr bool NETHER_DEBUG = false;
 #endif
 
+using namespace Microsoft::WRL;
+
 static inline void throw_if_failed(const HRESULT hr,
                                    const std::source_location source_location = std::source_location::current())
 {
@@ -47,6 +50,14 @@ static inline void throw_if_failed(const HRESULT hr,
         throw std::runtime_error(std::format("Exception caught at :: File name {}, Function name {}, Line number {}",
                                              source_location.file_name(), source_location.function_name(),
                                              source_location.line()));
+    }
+}
+
+static inline void set_name_d3d12_object(ComPtr<ID3D12Object> object, const std::wstring_view name)
+{
+    if constexpr (NETHER_DEBUG)
+    {
+        throw_if_failed(object->SetName(name.data()));
     }
 }
 
@@ -92,7 +103,7 @@ int main()
 
     try
     {
-        Microsoft::WRL::ComPtr<ID3D12Debug5> debug_layer = {};
+        ComPtr<ID3D12Debug5> debug_layer = {};
 
         // Enable the d3d12 debug layer in debug mode.
         uint32_t dxgi_factory_creation_flags = 0u;
@@ -110,11 +121,11 @@ int main()
         }
 
         // Create dxgi factory to get access to dxgi objects (like the swapchain and adapter).
-        Microsoft::WRL::ComPtr<IDXGIFactory6> dxgi_factory = {};
+        ComPtr<IDXGIFactory6> dxgi_factory = {};
         throw_if_failed(CreateDXGIFactory2(dxgi_factory_creation_flags, IID_PPV_ARGS(&dxgi_factory)));
 
         // Query the adapter (interface to the actual GPU).
-        Microsoft::WRL::ComPtr<IDXGIAdapter3> dxgi_adapter = {};
+        ComPtr<IDXGIAdapter3> dxgi_adapter = {};
         throw_if_failed(dxgi_factory->EnumAdapterByGpuPreference(0u, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,
                                                                  IID_PPV_ARGS(&dxgi_adapter)));
 
@@ -124,11 +135,12 @@ int main()
         std::wcout << std::format(L"Chosen adapter description :: {}", adapter_desc.Description) << std::endl;
 
         // Create the d3d12 device, which is required for creation of most objects in d3d12.
-        Microsoft::WRL::ComPtr<ID3D12Device5> device = {};
+        ComPtr<ID3D12Device5> device = {};
         throw_if_failed(D3D12CreateDevice(dxgi_adapter.Get(), D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&device)));
+        set_name_d3d12_object(device.Get(), L"D3D12 device");
 
         // Setup a info queue, so that breakpoints can be set when a message severity of a specific type comes up.
-        Microsoft::WRL::ComPtr<ID3D12InfoQueue> info_queue = {};
+        ComPtr<ID3D12InfoQueue> info_queue = {};
         if constexpr (NETHER_DEBUG)
         {
             throw_if_failed(device.As(&info_queue));
@@ -140,7 +152,7 @@ int main()
 
         // Query a d3d12 debug device to make sure all objects are properly cleared up and are not live at end of
         // application.
-        Microsoft::WRL::ComPtr<ID3D12DebugDevice2> debug_device = {};
+        ComPtr<ID3D12DebugDevice2> debug_device = {};
 
         if constexpr (NETHER_DEBUG)
         {
@@ -156,34 +168,45 @@ int main()
             .NodeMask = 0u,
         };
 
-        Microsoft::WRL::ComPtr<ID3D12CommandQueue> direct_command_queue = {};
+        ComPtr<ID3D12CommandQueue> direct_command_queue = {};
         throw_if_failed(device->CreateCommandQueue(&direct_command_queue_desc, IID_PPV_ARGS(&direct_command_queue)));
+        set_name_d3d12_object(direct_command_queue.Get(), L"D3D12 direct command queue");
 
         // Create the command allocators (i.e backing store for commands recorded via command lists).
-        Microsoft::WRL::ComPtr<ID3D12CommandAllocator> direct_command_allocator = {};
-        throw_if_failed(
-            device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&direct_command_allocator)));
+        std::array<ComPtr<ID3D12CommandAllocator>, NUM_BACK_BUFFERS> direct_command_allocators = {};
+        for (u32 i = 0; i < NUM_BACK_BUFFERS; i++)
+        {
+            throw_if_failed(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,
+                                                           IID_PPV_ARGS(&direct_command_allocators[i])));
+            set_name_d3d12_object(direct_command_allocators[i].Get(),
+                                  L"D3D12 direct command allocator" + std::to_wstring(i));
+        }
 
-        Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> graphics_command_list = {};
-        throw_if_failed(device->CreateCommandList(0u, D3D12_COMMAND_LIST_TYPE_DIRECT, direct_command_allocator.Get(),
-                                                  nullptr, IID_PPV_ARGS(&graphics_command_list)));
+        // Create command list.
+        ComPtr<ID3D12GraphicsCommandList> graphics_command_list = {};
+        throw_if_failed(device->CreateCommandList(0u, D3D12_COMMAND_LIST_TYPE_DIRECT,
+                                                  direct_command_allocators[0].Get(), nullptr,
+                                                  IID_PPV_ARGS(&graphics_command_list)));
+        throw_if_failed(graphics_command_list->Close());
+        set_name_d3d12_object(graphics_command_list.Get(), L"D3D12 Graphics command list");
 
         // Create sync primitives.
-        Microsoft::WRL::ComPtr<ID3D12Fence> fence = {};
+        ComPtr<ID3D12Fence> fence = {};
         throw_if_failed(device->CreateFence(0u, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence)));
+        set_name_d3d12_object(fence.Get(), L"D3D12 direct command queue fence");
 
         std::array<u64, NUM_BACK_BUFFERS> frame_fence_values = {};
         u64 current_fence_value = 0u;
 
         // Create the swapchain.
-        Microsoft::WRL::ComPtr<IDXGISwapChain1> swapchain = {};
+        ComPtr<IDXGISwapChain1> swapchain_1 = {};
         const DXGI_SWAP_CHAIN_DESC1 swapchain_desc = {
             .Width = CLIENT_WIDTH,
             .Height = CLIENT_HEIGHT,
             .Format = DXGI_FORMAT_R8G8B8A8_UNORM,
             .Stereo = false,
             .SampleDesc = {1u, 0u},
-            .BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT,
+            .BufferUsage = DXGI_USAGE_BACK_BUFFER,
             .BufferCount = NUM_BACK_BUFFERS,
             .Scaling = DXGI_SCALING_NONE,
             .SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD,
@@ -192,7 +215,10 @@ int main()
         };
 
         throw_if_failed(dxgi_factory->CreateSwapChainForHwnd(direct_command_queue.Get(), window_handle, &swapchain_desc,
-                                                             nullptr, nullptr, &swapchain));
+                                                             nullptr, nullptr, &swapchain_1));
+
+        ComPtr<IDXGISwapChain3> swapchain = {};
+        throw_if_failed(swapchain_1.As(&swapchain));
 
         // Create RTV descriptor heap, which is a contiguous memory allocation for render target views, which describe a
         // particular resource.
@@ -202,14 +228,40 @@ int main()
             .Flags = D3D12_DESCRIPTOR_HEAP_FLAGS::D3D12_DESCRIPTOR_HEAP_FLAG_NONE,
             .NodeMask = 0u,
         };
-        Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> rtv_descriptor_heap = {};
+        ComPtr<ID3D12DescriptorHeap> rtv_descriptor_heap = {};
         throw_if_failed(device->CreateDescriptorHeap(&rtv_descriptor_heap_desc, IID_PPV_ARGS(&rtv_descriptor_heap)));
+        set_name_d3d12_object(rtv_descriptor_heap.Get(), L"D3D12 rtv descriptor heap");
 
         // Create RTV for each of the swapchain backbuffer image.
+        struct BackBuffer
+        {
+            ComPtr<ID3D12Resource> resource{};
+            D3D12_CPU_DESCRIPTOR_HANDLE cpu_rtv_handle{};
+        };
+        std::array<BackBuffer, NUM_BACK_BUFFERS> back_buffers = {};
+
+        D3D12_CPU_DESCRIPTOR_HANDLE current_rtv_descriptor_handle =
+            rtv_descriptor_heap->GetCPUDescriptorHandleForHeapStart();
+
+        SIZE_T rtv_descriptor_handle_increment_size =
+            device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+        for (u32 i = 0; i < NUM_BACK_BUFFERS; i++)
+        {
+            throw_if_failed(swapchain->GetBuffer(i, IID_PPV_ARGS(&back_buffers[i].resource)));
+
+            device->CreateRenderTargetView(back_buffers[i].resource.Get(), nullptr, current_rtv_descriptor_handle);
+
+            back_buffers[i].cpu_rtv_handle = current_rtv_descriptor_handle;
+
+            current_rtv_descriptor_handle.ptr += rtv_descriptor_handle_increment_size;
+        }
 
         ShowWindow(window_handle, SW_SHOW);
 
         // Main game loop.
+        u32 current_swapchain_backbuffer_index = swapchain->GetCurrentBackBufferIndex();
+
         bool quit = false;
         while (!quit)
         {
@@ -230,6 +282,65 @@ int main()
 
                 TranslateMessage(&message);
                 DispatchMessage(&message);
+            }
+
+            // Reset command allocator and list for current frame.
+            throw_if_failed(direct_command_allocators[current_swapchain_backbuffer_index]->Reset());
+            throw_if_failed(graphics_command_list->Reset(
+                direct_command_allocators[current_swapchain_backbuffer_index].Get(), nullptr));
+
+            // Transition the swapchain backbuffer from a presentable format to render target view.
+            const D3D12_RESOURCE_BARRIER backbuffer_present_to_render_target = {
+                .Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
+                .Transition =
+                    {
+
+                        .pResource = back_buffers[current_swapchain_backbuffer_index].resource.Get(),
+                        .Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES,
+                        .StateBefore = D3D12_RESOURCE_STATE_PRESENT,
+                        .StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET,
+                    },
+            };
+            graphics_command_list->ResourceBarrier(1u, &backbuffer_present_to_render_target);
+
+            static const std::array<f32, 4> clear_color{1.0f, 0.0f, 0.0f, 1.0f};
+            graphics_command_list->ClearRenderTargetView(
+                back_buffers[current_swapchain_backbuffer_index].cpu_rtv_handle, clear_color.data(), 0u, nullptr);
+
+            // Transition swapchain back to presentable format.
+            const D3D12_RESOURCE_BARRIER backbuffer_render_target_backbuffer_to_present = {
+                .Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
+                .Transition =
+                    {
+
+                        .pResource = back_buffers[current_swapchain_backbuffer_index].resource.Get(),
+                        .Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES,
+                        .StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET,
+                        .StateAfter = D3D12_RESOURCE_STATE_PRESENT,
+                    },
+            };
+            graphics_command_list->ResourceBarrier(1u, &backbuffer_present_to_render_target);
+
+            // Submit command list for execution.
+            throw_if_failed(graphics_command_list->Close());
+
+            ID3D12CommandList *const command_lists_to_execute[] = {
+                graphics_command_list.Get(),
+            };
+
+            direct_command_queue->ExecuteCommandLists(1u, command_lists_to_execute);
+
+            // Present & signal.
+            throw_if_failed(swapchain->Present(1u, 0u));
+
+            current_fence_value++;
+            throw_if_failed(direct_command_queue->Signal(fence.Get(), current_fence_value));
+            frame_fence_values[current_swapchain_backbuffer_index] = current_fence_value;
+
+            current_swapchain_backbuffer_index = swapchain->GetCurrentBackBufferIndex();
+            if (fence->GetCompletedValue() < frame_fence_values[current_swapchain_backbuffer_index])
+            {
+                fence->SetEventOnCompletion(frame_fence_values[current_swapchain_backbuffer_index], nullptr);
             }
         }
     }
